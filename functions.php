b2d1ea13df77a75187ac8edfae6a3f80351da294
Tony Ciccarone <tonyvciccarone@gmail.com>
2023-02-10 06:23:23 -0500

    commit

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 if ( ! function_exists('V_enqueue_styles') ) {
     function V_enqueue_styles() {
         wp_enqueue_style( 'twenty-twenty-three-style', get_template_directory_uri() .'/style.css' );
+        wp_enqueue_style( 'v-style', get_stylesheet_directory_uri() .'/style.css' );
     }
     add_action('wp_enqueue_scripts', 'V_enqueue_styles');
 }
